eec107446d596c77d77251bb2db138facff0dc83
Jan Vavro <vavrojj@gmail.com>
2020-02-28 16:28:58 +0100

    edited makefile added support for port in argument

--- sem_6/ipk/src/Main.java
+++ sem_6/ipk/src/Main.java
@@ -1,13 +1,31 @@
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.util.Date;
+import java.util.Arrays;
+
+import static java.lang.System.exit;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        //TODO process arguments
-        int port = 5353;
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("ERROR: Wrong arguments, should run only with one argument <port_number>\nin range <1024,65353>");
+            exit(1);
+        }
+        int port = -1;
+
+        try {
+            port = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.err.println("ERROR: Wrong arguments, should run only with one argument <port_number>\nin range <1024,65353>");
+            exit(1);
+        }
+        if (port < 0) {
+            System.err.println("ERROR: Wrong arguments, port has to be positive number\nin range <1024,65353>");
+            exit(1);
+        } else if (port < 1024) {
+            System.err.println("ERROR: Wrong arguments, reserved port\nport has to be in range <1024,65353>");
+            exit(1);
+        } else if (port > 65353) {
+            System.err.println("ERROR: Wrong arguments\nport has to be in range <1024,65353>");
+            exit(1);
+        }
         Server server = new Server(port);
         server.run();
     }
